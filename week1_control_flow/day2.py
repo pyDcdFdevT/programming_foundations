634fe93128e32c9bab15c8d6c0f7d2e598d1057e
pyDcdFdevT <diegofelipetv97@gmail.com>
2026-02-16 18:51:56 +0100

    Day 2: while loop mechanics, continue, order of execution

---
+++ week1_control_flow/day2.py
@@ -0,0 +1,11 @@
+contador = 0 
+
+while contador < 10:
+    contador +=1
+    if contador == 5:
+        continue
+    print(contador)
+    
+while contador < 5:
+    print(contador)
+    contador += 1
